d9e4a33a80cdbbe2ddddbf69fe7d1a1582dde5cb
Your Name <test@example.com>
2020-09-14 20:36:09 +0300

    commit

--- index.php
+++ index.php
@@ -16,5 +16,7 @@ $title = 'lesson53git';
 <!-- 
 
 git remote - проверить существует ли связь с удаленным репозиторием
-    
+git branch - создание ветки
+git checkout - переключение между ветками
+git log --oneline --decorate --graph --all - вывести все ветки 
  -->
